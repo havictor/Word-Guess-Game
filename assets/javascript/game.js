861c1deb6dd4a539e5f23ebcd0cdf911e3760c86
Ha <victorha@gmail.com>
2018-04-24 02:24:14 -0400

    Pseudo Coded plans version 1

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+var winCounter = 0;
+var word = ["Bulbasaur", "Ivysaur", "Venusaur"];
+var wordChoice = "";
+var guessCounter = 12;
+var guessLetters = [];
+var guessWord = "";
+
+
+
+
+
+//onpress event of keyboard 
+
+//guessWord changes (to add the new letters) with each letter guess,  tied to onpress event
+//guessCounter decreases
+//push letter to guessLetters
+
+
+
+
+//win condition:
+//guess === word
+//increase win count
+//clears guessLetters
+//chooses new wordChoice from word (math.random())
+//guessCounter = 12
+
+//else  lose condition (guessCounter === 0)
+//clears guessLetters
+//choose new wordChoice from word (math.random())
+
+
+
+
+
+
+/*right column.
+Press any key to get started
+Wins: <count>
+Current Word:
+______________
+Number of Guesses:
+<Count decreasing>
+Letters already guessed:
+<Letters Guessed>
+*/
