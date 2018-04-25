92cece9fa5ff77569d52b37c1b2c8e296573521c
Ha <victorha@gmail.com>
2018-04-25 18:21:00 -0400

    Loop Error With Already Used Letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,61 @@
 var winCounter = 0;
-var pokedex = ["BULBASAUR", "IVYSAUR", "VENUSAUR"];
+var pokedex = ["BULBASAUR", "IVYSAUR", "VENUSAUR", "CHARMANDER", "CHARMELEON", "CHARIZARD", "SQUIRTLE", "WARTORTLE", "BLASTOISE"];
 var wordChoice = "";
 var guessCounter = 12;
 var guessLetters = [];
 var guessWord = [];
 var guessStatus = [];
+var press = "";
 
-newGame();
+window.onload = function() {
+    newGame();
+}
 
 function newGame() {
     guessLetters = [];
+    guessStatus = [];
     var n = Math.floor((Math.random() * pokedex.length));
-    wordChoice = pokedex[n]; //needs to push word into new 
+    wordChoice = pokedex[n];
     guessWord = wordChoice.split("");
     for (i=0; i < guessWord.length; i++) {
         guessStatus.push("_");
-    }
+    };
+    document.getElementById("guessStatus").innerHTML=guessStatus;
     guessCounter = 12;
 };
 
 
-window.addEventListener("keydown", function (e) {
-    var press = e.toUpperCase;
-    //alert(press);
-    for (j = 0; j < guessLetters.length; j++) {
+document.addEventListener("keypress", function(e) {
+    press = event.key.toUpperCase();
+    for (j = -1; j < guessLetters.length; j++) {
         if (press !== guessLetters[j]) {
+
+
+            guessLetters.push(press);
+            guessCounter -= 1;  
             for (k = 0; k < guessWord; k++) {
+ 
+                
                 if (press === guessWord[k]) {
                     guessStatus[k] = guessWord[k];
-                    guessCounter -= 1;
                 }
-                else guessStatus.push(press);
-                guessCounter -= 1;
             }
+            alert(press);
         }
     }
+    document.getElementById("guessLetters").innerHTML = guessLetters;
+    document.getElementById("guessStatus").innerHTML = guessStatus;
+    document.getElementById("guessCounter").innerHTML = guessCounter;
 });
 
-if (guessStatus === wordChoice) {
+if (guessStatus.toString === wordChoice.toString) {
     alert("Congratulations, you win");
     winCounter += 1;
+    document.getElementById("winCounter").innerHTML = "Wins: "+winCounter;
     newGame();
 };
 
-if (guessCounter === 0) {
+if (guessCounter <= 0) {
     alert("Sorry, you're out of guesses")
     newGame();
 };
-
-//onpress event of keyboard 
-
-//guessWord changes (to add the new letters) with each letter guess,  tied to onpress event
-//guessCounter decreases
-//push letter to guessLetters
-
-
-
-
-//win condition:
-//guess === word
-//increase win count
-//clears guessLetters
-//chooses new wordChoice from word (math.random())
-//guessCounter = 12
-
-//else  lose condition (guessCounter === 0)
-//clears guessLetters
-//choose new wordChoice from word (math.random())
-
-
-
-
-
-
-/*right column.
-Press any key to get started
-Wins: <count>
-Current Word:
-______________
-Number of Guesses:
-<Count decreasing>
-Letters already guessed:
-<Letters Guessed>
-*/
