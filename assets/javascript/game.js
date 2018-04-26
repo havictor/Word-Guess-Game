7d13dbeceb5088c3ac54d90d22bc9f926c757ffa
Ha <victorha@gmail.com>
2018-04-25 23:20:19 -0400

    Added images, tidied code a bit, winning images not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var guessWord = [];
 var guessStatus = [];
 var press = "";
 var rightCounter = 0;
+var pokedexOfChoice = 0;
 
 window.onload = function() {
     newGame();
@@ -17,6 +18,7 @@ function newGame() {
     guessStatus = [];
     var n = Math.floor((Math.random() * pokedex.length));
     wordChoice = pokedex[n];
+    pokedexOfChoice = n + 1;
     guessWord = wordChoice.split("");
     for (i=0; i < guessWord.length; i++) {
         guessStatus.push("_");
@@ -27,48 +29,45 @@ function newGame() {
     document.getElementById("guessCounter").innerHTML = guessCounter;
 };
 
+/*
+function winningImage() {
+    var image = document.createAttribute("IMG");
+    image.setAttribute("src", "pokedexOfChoice+".png"");
+    document.getElementById("winner").innerHTML;
 
+}*/
 
 document.addEventListener("keypress", function(e) {
     press = event.key.toUpperCase();
-//for (j = 0; j < guessLetters.length; j++) { //examine previous guesses to not count duplicates
-//            if (press !== guessLetters[j]) { //continue if not 
-                guessLetters.push(press);                
-                for (k = 0; k < guessWord.length; k++) {
-                
-                    if (press == guessWord[k]) {
-                        guessStatus[k] = press;
-                        document.getElementById("guessStatus").innerHTML = guessStatus;
-                        rightCounter += 1;
-                    }
-                }
-
-//            }
-//            else {
-            if (rightCounter == 0) {
-                guessCounter -= 1;
-            }
-            else {
-                rightCounter = 0
-            }
-//            }
-//        }   
+    guessLetters.push(press);                
+    for (k = 0; k < guessWord.length; k++) {
+        if (press == guessWord[k]) {
+            guessStatus[k] = press;
+            document.getElementById("guessStatus").innerHTML = guessStatus;
+            rightCounter += 1;
+        }
+    }
+        if (rightCounter == 0) {
+            guessCounter -= 1;
+        }
+        else {
+            rightCounter = 0
+        } 
     document.getElementById("guessLetters").innerHTML = guessLetters;
     document.getElementById("guessStatus").innerHTML = guessStatus;
     document.getElementById("guessCounter").innerHTML = guessCounter;
 
+    if (guessStatus.toString() == guessWord.toString()) {
+        alert("Congratulations, you win");
+        winCounter += 1;
+        document.getElementById("winCounter").innerHTML = "Wins: "+winCounter;
+//        winningImage();
+        newGame();
+    };
 
-if (guessStatus.toString() == guessWord.toString()) {
-    alert("Congratulations, you win");
-    winCounter += 1;
-    document.getElementById("winCounter").innerHTML = "Wins: "+winCounter;
-    newGame();
-};
-
-if (guessCounter <= 0) {
-    alert("Sorry, you're out of guesses");
-    newGame();
-}
-
+    if (guessCounter <= 0) {
+        alert("Sorry, you're out of guesses");
+        newGame();
+    }
 });
 
